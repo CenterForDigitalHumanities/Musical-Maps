78a52fcac1f4f5123a303ff77c7b202ac63b0563
Patrick Cuba <cubap@slu.edu>
2023-05-09 16:59:49 -0500

    new files

---
+++ js/deer-form.js
@@ -0,0 +1,45 @@
+/**
+ * @module DEER Data Encoding and Exhibition for RERUM (DEER)
+ * @author Patrick Cuba <cubap@slu.edu>
+ * @author Bryan Haberberger <bryan.j.haberberger@slu.edu>
+ * @version 0.11
+
+ * This code should serve as a basis for developers wishing to
+ * use TinyThings as a RERUM proxy for an application for data entry,
+ * especially within the Eventities model.
+ * @see tiny.rerum.io
+ */
+
+// Identify an alternate config location or only overwrite some items below.
+import { default as DEER } from 'https://centerfordigitalhumanities.github.io/deer/releases/alpha-0.11/deer-config.js'
+
+// Overwrite or add certain values to the configuration to customize.
+
+// new template
+DEER.TEMPLATES.cat = (obj) => `<h5>${obj.name}</h5><img src="http://placekitten.com/300/150" style="width:100%;">`
+
+// sandbox repository URLS
+DEER.URLS = {
+    BASE_ID: "https://devstore.rerum.io/v1",
+    CREATE: "//tinydev.rerum.io/app/create",
+    UPDATE: "//tinydev.rerum.io/app/update",
+    QUERY: "//tinydev.rerum.io/app/query",
+    OVERWRITE: "//tinydev.rerum.io/app/overwrite",
+    SINCE: "//devstore.rerum.io/v1/since"
+}
+// Render is probably needed by all items, but can be removed.
+// CDN at https://centerfordigitalhumanities.github.io/deer/releases/
+import { default as renderer, initializeDeerViews } from 'https://centerfordigitalhumanities.github.io/deer/releases/alpha-0.11/deer-render.js'
+
+// Record is only needed for saving or updating items.
+// CDN at https://centerfordigitalhumanities.github.io/deer/releases/
+import { default as record, initializeDeerForms } from 'https://centerfordigitalhumanities.github.io/deer/releases/alpha-0.11/deer-record.js'
+
+// fire up the element detection as needed
+try {
+    initializeDeerViews(DEER)
+    initializeDeerForms(DEER)
+} catch (err) {
+    // silently fail if render or record is not loaded
+}
+
---
+++ js/main.js
@@ -0,0 +1,7 @@
+function loadHashId(){
+    const hash = location.hash
+    if(!hash){ return }
+    if(hash.length===24){ hash = `https://devstore.rerum.io/v1/id/${hash}`}
+    if(!hash.startsWith('http')) { return }
+    document.querySelectorAll('[hash-id]').forEach(el=>el.setAttribute('deer-id',hash))
+}
